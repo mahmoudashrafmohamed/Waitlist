4badf10f8425ccef6767193100d217dc43036d08
mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2017-07-09 10:31:23 +0200

    Fill the database with fake data and update Recycler

--- app/src/main/java/com/mahmoud_ashraf/waitlistapp/GuestListAdapter.java
+++ app/src/main/java/com/mahmoud_ashraf/waitlistapp/GuestListAdapter.java
@@ -14,14 +14,16 @@ import android.widget.TextView;
 public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.GuestViewHolder> {
 
     private Context mContext;
+    private int mCount;
 
     /**
      * Constructor using the context and the db cursor
      *
      * @param context the calling context/activity
      */
-    public GuestListAdapter(Context context) {
+    public GuestListAdapter(Context context, int count) {
         this.mContext = context;
+        this.mCount = count;
     }
 
     @Override
@@ -40,7 +42,7 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mCount;
     }
 
 
